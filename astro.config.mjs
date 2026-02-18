cba5f308bab9e324083bab6709d9c63d0aa2f897
dvankevich <dvankevich@gmail.com>
2026-02-18 22:03:18 +0200

    build: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import preact from "@astrojs/preact";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: netlify(),
   integrations: [preact(), icon()],
 });
